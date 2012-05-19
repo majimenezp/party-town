88c466574d5bb09ea0b2de203aeaea6b6fb5392e
Joel Garcia Martinez <joelgarciamartinez@gmail.com>
2012-05-19 11:08:31 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PartyTown::Application.config.secret_token = '10fa8d14e72aeafb611d986c2ed6b777728799f265b24f155538d7fb5c8268497cf61382aa002bb16b4caad9d1a33a5aecba05b25f772d75143ee63def15e83a'
