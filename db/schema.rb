a1133f6109dfc49b2bf917a86ad8c75319cafe35
Joel Garcia Martinez <joelgarciamartinez@gmail.com>
2012-05-19 12:29:30 -0500

    Events migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20120519172739) do
+
+  create_table "events", :force => true do |t|
+    t.string   "title"
+    t.string   "address_line_1"
+    t.string   "address_line_2"
+    t.string   "city"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "when"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
 
 end
