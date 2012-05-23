c06bf6fdcfc571b4af22cdcd1a34e948ca09d766
Joel Garcia Martinez <joelgarciamartinez@gmail.com>
2012-05-23 17:31:16 -0500

    The same but different

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_filter :authenticate_user!, only: [:new, :edit, :create, :update]
+  before_filter :authenticate_user!, except: [:index, :show]
   # GET /events
   # GET /events.json
   def index
