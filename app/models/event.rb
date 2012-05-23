86700a8e08e1d7d5c16da5fe5372a5b428fe46e2
Joel Garcia Martinez <joelgarciamartinez@gmail.com>
2012-05-23 16:21:08 -0500

    better urls

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ActiveRecord::Base
   attr_accessible :address_line_1, :address_line_2, :city, :latitude, :longitude, :title, :when, :hashtag
+
+  def to_param
+    "#{id}-#{title.parameterize}"
+  end
 end
